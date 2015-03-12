95bdbf0c4e7fbd3a29c5b0b2bc84a878acfae2d0
Andi Nitsche <andi@inbaz.org>
2015-03-12 13:33:30 +0100

    deactivated new request error log
    deactivated new request error log

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
  * to the application root now.
  */
 chdir(dirname(__DIR__));
-error_log('===== new request =====');
+#error_log('===== new request =====');
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
