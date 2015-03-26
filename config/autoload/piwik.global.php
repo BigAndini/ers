3ba44ba2aa0a6497aa55c6c508167cdf7506406c
Andi Nitsche <andi@inbaz.org>
2015-03-26 13:12:50 +0100

    added piwik support
    added piwik support

---
+++ config/autoload/piwik.global.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+# copy this file to piwik.local.php
+# uncomment the return
+# do additional changes in piwik.local.php
+
+
+/*return array(
+    'orgHeiglPiwik' => array(
+
+        // Always omit a trailing slash!
+        'server' => 'prereg.eja.net/analytics',
+        'site_id' => 1,
+    ), 
+);*/
+return array();
