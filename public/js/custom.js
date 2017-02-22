f353706f9a639d35d70b4d9a59f0734cd8d72bd1
Andi Nitsche <andi@inbaz.org>
2017-02-22 22:03:57 +0100

    sourceMappingURL pragmas is deprecated; added makeid function to custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -7,6 +7,17 @@
 
 // /public/js/custom.js
 
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 6; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 jQuery(function($) {
     $(window).focus(function(){
         if(window.sessionStorage) {
