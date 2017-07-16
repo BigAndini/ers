07a62fa2ee8a6062510f2e0b152ff08cbea6304d
Andi Nitsche <andi@sixhop.net>
2017-07-16 23:21:13 +0200

    fixed codacy issues

--- data/testing.sql
+++ data/testing.sql
@@ -1,6 +1,6 @@
-; add default user: user@ers.inbaz.org
-; add onsitereg user: onsite@ers.inbaz.org
-; add admin user: admin@ers.inbaz.org
-; tax examples
-; deadline examples
-; agegroup examples
+-- add default user: user@ers.inbaz.org
+-- add onsitereg user: onsite@ers.inbaz.org
+-- add admin user: admin@ers.inbaz.org
+-- tax examples
+-- deadline examples
+-- agegroup examples
