b2f70440eba012a23ef18f0e6364f30fc8401042
Andi Nitsche <andi@sixhop.net>
2017-11-17 09:02:21 +0100

    updated initial sql; removed remember me

--- data/initial.sql
+++ data/initial.sql
@@ -15,13 +15,17 @@ INSERT INTO `role` (`id`, `parent_id`, `roleId`, `active`, `updated`, `created`)
 -- agegroup examples
 
 -- add status
-INSERT INTO `status` (`id`, `position`, `value`, `description`, `updated`, `created`, `active`) VALUES
-(1, 1, 'order pending', '', NULL, NULL, 0),
-(2, 2, 'ordered', '', NULL, NULL, 1),
-(3, 3, 'paid', '', NULL, NULL, 1),
-(4, 4, 'shipped', '', NULL, NULL, 1),
-(5, 5, 'cancelled', '', NULL, NULL, 0),
-(6, 6, 'transferred', '', NULL, NULL, 0);
+INSERT INTO `status` (`id`, `position`, `active`, `value`, `description`, `updated`, `created`, `valid`) VALUES
+(1, 1, 0, 'order pending', '', NULL, NULL, 0),
+(2, 2, 1, 'ordered', '', NULL, NULL, 0),
+(3, 3, 1, 'paid', '', NULL, NULL, 1),
+(4, 4, 1, 'shipped', '', NULL, NULL, 0),
+(5, 5, 0, 'cancelled', '', NULL, NULL, 0),
+(6, 6, 0, 'transferred', '', NULL, NULL, 0),
+(7, 3, 1, 'free', '', NULL, NULL, 1),
+(8, 3, 1, 'bar', '', NULL, NULL, 1),
+(9, 5, 1, 'overpaid', '', NULL, NULL, 0),
+(10, 3, 1, 'partly paid', '', NULL, NULL, 0);
 
 INSERT INTO `user` (`id`, `username`, `email`, `email_status`, `display_name`, `firstname`, `surname`, `gender`, `Country_id`, `password`, `hashkey`, `state`, `active`, `birthday`, `login_count`, `newsletter`, `updated`, `created`) VALUES 
 (NULL, NULL, 'andi@inbaz.org', NULL, NULL, 'Andi', 'Nitsche', NULL, NULL, NULL, NULL, NULL, '1', NULL, NULL, NULL, '2017-02-11 00:00:00', '2017-02-11 00:00:00');
